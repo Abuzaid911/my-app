492ce264ff26a641ecfa858c46341e378c64d1a1
Ahmed Abuzaid <sabobee911@gmail.com>
2023-01-05 17:22:06 +0200

    deleting border

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -8,7 +8,7 @@ function About() {
 			<div className="flex flex-col md:flex-row items-center font-mono justify-center gap-10 md:gap-20 py-12  text-gray-600 dark:text-indigo-400 ">
 				<div className="md:w-6/12">
 					<SectionTitle>About Me</SectionTitle>
-					<p className="text-md text-gray-700 dark:text-gray-400 font-mono border-solid border-2" >
+					<p className="text-md text-gray-700 dark:text-gray-400 font-mono" >
 						I'm a 22 year old student studying Information Systems. I'm passionate about web development and design, and am always looking for new ways to expand my knowledge and skillset in the tech world. I'm excited to see how technology can be used to create solutions to everyday problems and to create experiences that make life more enjoyable.
 					</p>
 
